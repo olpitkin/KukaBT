f6dd0b5c18869a0b8b07e91c997474c578a3987e
Alex Pitkin <alex@sneedleapp.com>
2016-06-28 23:32:40 +0200

    1 button fix

--- app/src/main/java/kuka/kuka_bluetooth/MainActivity.java
+++ app/src/main/java/kuka/kuka_bluetooth/MainActivity.java
@@ -1,48 +1,72 @@
 package kuka.kuka_bluetooth;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnTouch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+
+    AtomicBoolean actionDownFlag = new AtomicBoolean(true);
+
+    Handler handler;
+    List<Thread> tasks = new ArrayList<>();
+
+    // Define the code block to be executed
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+            // Do something here on the main thread
+            Log.d("Handlers", "Called on main thread");
+            for (Thread t : tasks){
+
+                if (t.getState() == Thread.State.NEW)
+                {
+                    t.start();
+                }
+
+            }
+            tasks.clear();
+
+            handler.postDelayed(runnableCode, 2000);
+        }
+    };
 
     @BindView(R.id.startButton) Button mStartButton;
     @BindView(R.id.stopButton) Button mStopButton;
     @BindView(R.id.sendButton) Button mSendButton;
 
-    @OnTouch(R.id.ul)
-    public void ul(View view) {
-        try {
-            mBluetoothServer.send("u".getBytes());
-        } catch (BluetoothServer.BluetoothServerException | IOException e) {
-            e.printStackTrace();
-        }
-    }
+    @BindView(R.id.ul) Button ul;
 
     @OnTouch(R.id.up)
-    public void up(View view) {
+    public boolean up(View view) {
         try {
             mBluetoothServer.send("i".getBytes());
+            return true;
         } catch (BluetoothServer.BluetoothServerException | IOException e) {
             e.printStackTrace();
         }
+        return false;
     }
 
     @OnTouch(R.id.ur)
     public boolean ur(View view) {
         try {
             mBluetoothServer.send("o".getBytes());
-            return true;
+            return false;
         } catch (BluetoothServer.BluetoothServerException | IOException e) {
             e.printStackTrace();
         }
@@ -53,8 +77,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean left(View view) {
         try {
             mBluetoothServer.send("j".getBytes());
+            mBluetoothServer.wait(1000);
             return true;
         } catch (BluetoothServer.BluetoothServerException | IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return false;
@@ -66,8 +93,7 @@ public class MainActivity extends AppCompatActivity {
             mBluetoothServer.send("l".getBytes());
             return true;
         } catch (BluetoothServer.BluetoothServerException | IOException e) {
-            e.printStackTrace();
-        }
+            e.printStackTrace();}
         return false;
     }
 
@@ -114,6 +140,48 @@ public class MainActivity extends AppCompatActivity {
 
         mBluetoothServer = new BluetoothServer();
         mBluetoothServer.setListener(mBluetoothServerListener);
+
+        ul.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Thread t =  new Thread(new Runnable(){
+                    public void run(){
+                        while(actionDownFlag.get()){
+                            if (tasks.isEmpty()) {
+                                tasks.add(new Thread(new Runnable(){
+                                    public void run(){
+                                        try {
+                                            mBluetoothServer.send("u".getBytes());
+                                        } catch (BluetoothServer.BluetoothServerException | IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }));
+                            }
+                            //maybe sleep some times to not polute your logcat
+                        }
+                        //Log.d(VIEW_LOG_TAG, "Not Touching");
+                    }
+                });
+
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                    actionDownFlag.set(true);
+                    t.start();
+
+
+                }
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    actionDownFlag.set(false);
+                }
+                return true;
+            }
+        });
+
+
+        handler = new Handler();
+// Start the initial runnable task by posting through the handler
+        handler.post(runnableCode);
 
     }
 
