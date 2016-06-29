ff257ab343cc8175885ab5acb5b5fa8ebac65c8d
Alex Pitkin <alex@sneedleapp.com>
2016-06-29 15:27:28 +0200

    Joystick (no functionality)

--- app/src/main/java/kuka/kuka_bluetooth/MainActivity.java
+++ app/src/main/java/kuka/kuka_bluetooth/MainActivity.java
@@ -7,6 +7,9 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,6 +27,12 @@ public class MainActivity extends AppCompatActivity{
 
     Handler handler;
     List<Thread> tasks = new ArrayList<>();
+
+
+    @BindView(R.id.layout_joystick) RelativeLayout layout_joystick;
+    @BindView(R.id.textView5) TextView textView5;
+    JoyStickClass js;
+    ImageView image_joystick, image_border;
 
     // Define the code block to be executed
     private Runnable runnableCode = new Runnable() {
@@ -69,6 +78,56 @@ public class MainActivity extends AppCompatActivity{
 
         mBluetoothServer = new BluetoothServer();
         mBluetoothServer.setListener(mBluetoothServerListener);
+
+        js = new JoyStickClass(getApplicationContext(), layout_joystick, R.drawable.image_button);
+        js.setStickSize(150, 150);
+        js.setLayoutSize(500, 500);
+        js.setLayoutAlpha(150);
+        js.setStickAlpha(100);
+        js.setOffset(90);
+        js.setMinimumDistance(50);
+
+        layout_joystick.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                js.drawStick(arg1);
+                if(arg1.getAction() == MotionEvent.ACTION_DOWN
+                        || arg1.getAction() == MotionEvent.ACTION_MOVE) {
+/*                    textView1.setText("X : " + String.valueOf(js.getX()));
+                    textView2.setText("Y : " + String.valueOf(js.getY()));
+                    textView3.setText("Angle : " + String.valueOf(js.getAngle()));
+                    textView4.setText("Distance : " + String.valueOf(js.getDistance()));*/
+
+                    int direction = js.get8Direction();
+                    if(direction == JoyStickClass.STICK_UP) {
+                        textView5.setText("Direction : Up");
+                    } else if(direction == JoyStickClass.STICK_UPRIGHT) {
+                        textView5.setText("Direction : Up Right");
+                    } else if(direction == JoyStickClass.STICK_RIGHT) {
+                        textView5.setText("Direction : Right");
+                    } else if(direction == JoyStickClass.STICK_DOWNRIGHT) {
+                        textView5.setText("Direction : Down Right");
+                    } else if(direction == JoyStickClass.STICK_DOWN) {
+                        textView5.setText("Direction : Down");
+                    } else if(direction == JoyStickClass.STICK_DOWNLEFT) {
+                        textView5.setText("Direction : Down Left");
+                    } else if(direction == JoyStickClass.STICK_LEFT) {
+                        textView5.setText("Direction : Left");
+                    } else if(direction == JoyStickClass.STICK_UPLEFT) {
+                        textView5.setText("Direction : Up Left");
+                    } else if(direction == JoyStickClass.STICK_NONE) {
+                        textView5.setText("Direction : Center");
+                    }
+                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
+/*                    textView1.setText("X :");
+                    textView2.setText("Y :");
+                    textView3.setText("Angle :");
+                    textView4.setText("Distance :");*/
+                    textView5.setText("Direction :");
+                }
+                return true;
+            }
+        });
+
 
         initListners();
         handler = new Handler();
